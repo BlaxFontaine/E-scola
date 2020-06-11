78d8ead7eadec4bd5013105ecb08ced9b2e4700e
maxime <maxime.goguet@epitech.eu>
2020-06-11 17:29:37 +0200

    Correction Migration

--- back/src/Migrations/Version20200607212845.php
+++ back/src/Migrations/Version20200607212845.php
@@ -21,7 +21,7 @@ final class Version20200607212845 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
+        $this->addSql('CREATE TABLE mascot (id INT AUTO_INCREMENT NOT NULL, state VARCHAR(255) NOT NULL, sentence VARCHAR(255) NOT NULL, img VARCHAR(255) NOT NULL , PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE activities (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE lessons (id INT AUTO_INCREMENT NOT NULL, activity_id INT NOT NULL, name VARCHAR(255) NOT NULL, start DATE NOT NULL, end DATE NOT NULL, INDEX IDX_3F4218D981C06096 (activity_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE lessons ADD CONSTRAINT FK_3F4218D981C06096 FOREIGN KEY (activity_id) REFERENCES activities (id)');
