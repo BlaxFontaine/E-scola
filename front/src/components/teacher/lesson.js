8a56e7e0ca0f56b05837b1e1cc306786f7ac70e7
maxime <maxime.goguet@epitech.eu>
2020-06-12 12:04:42 +0200

    ADD lessons

--- front/src/components/teacher/lesson.js
+++ front/src/components/teacher/lesson.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-
-
-export default class Student extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            lesson: []
-        }
-        this.onEdit = this.onEdit.bind(this)
-    }
-
-    onEdit() {
-        console.log(this.state.lesson)
-    }
-
+import {Card} from 'react-bootstrap'
+import Lesson1 from '../../assets/img/png/teacher/materials/unnamed.png'
+import Lesson2 from '../../assets/img/png/teacher/materials/unnamed2.png'
+import Lesson3 from '../../assets/img/png/teacher/materials/unnamed3.png'
+import {Container,Row,Col} from 'react-grid-system'
+export default class Lesson extends Component {
+    
     render() {
         return (
-            <tbody>
-            <tr>
-              <td>{this.props.title}</td>
-              <td>{this.props.start_date}</td>
-              <td>{this.props.end_date}</td>
-            </tr>
-            </tbody>
+            <Container >
+            <br/>
+            <br/>
+            <Row>
+            <Col xl={4}>
+            <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={Lesson1} width={300} />
+            <Card.Body>
+              <Card.Title>Le son m</Card.Title>
+              <Card.Text>
+                Leçon 1
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          </Col>
+          <Col xl={4}>
+          <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={Lesson2} width={300} />
+            <Card.Body>
+              <Card.Title>Le son m</Card.Title>
+              <Card.Text>
+                Leçon 2
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          </Col>
+          <Col xl={4}>
+          <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={Lesson3} width={300} />
+            <Card.Body>
+              <Card.Title>Le son m</Card.Title>
+              <Card.Text>
+                Leçon 3
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          </Col>
+          </Row>
+          </Container>
+            
             
         )
     }
